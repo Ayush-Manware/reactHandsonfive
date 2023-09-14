ecc3abdd25abc83fef0718de6ef213b826103778
Ayush Manware <ayushmanware19@gmail.com>
2023-09-14 13:19:06 +0530

    disription of components and examples

--- src/HOC.jsx
+++ src/HOC.jsx
@@ -12,13 +12,14 @@ const Main = ()=>{
       };
     return(
         <>
+        <h4>
+          Higher order components are the components which can be used globally or we can say using components as props. In the example below, I have created three functions. Second function returns "Please Login", Third component returns "Logged In" and in the First function I have declared a state which is false and created a toggle button with this whose value fluctuate on click. The Main function is returning component three if the value is true, and returns component two if the value is false.
+        </h4>
          <CompoOne />
          <button onClick={()=>{setloggedIn(!loggedIn)}}>Change State</button>
         </>
     )
 }
-
-
 
 const CompoTwo = () => {
   return (
--- src/PureCompo.jsx
+++ src/PureCompo.jsx
@@ -13,11 +13,15 @@ export default class PureCompo extends PureComponent {
     console.log(this.state.count);
     return (
       <>
+      <h4>
+        Pure component stops the re-rendering of a component untill the state is changed. If the state is changed then only the component will render otherwise it will remain same. In the following example checkout console.log for the results, everytime Update button is clicked the state of the component changes and the component re-renders itself. Overall pure component renders the component only if the state is getting changed somehow, otherwise it will not re-render.
+      </h4>
         <h1>This is Pure Component : {this.state.count}</h1>
         <button onClick={() => this.setState({ count: this.state.count + 3 })}>
           Update
         </button>
-        <p>Pure component is stopping the code to execute multiple times. If the state changes then only the component will render.</p>
+
+        <hr />
       </>
     );
   }
